b985fb498c0b62d8a9670cdb042a676272a552f2
Naveen Prashanth KS <140269843+naveenprashanthks@users.noreply.github.com>
2025-06-14 09:30:58 +0530

    script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Btech.AI & DS student','Data Scientist','Full stack developer'],
+    strings:['Freelancer','AI Engineer','Full stack developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
